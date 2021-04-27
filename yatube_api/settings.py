f99504e63f66b6cdbca34c67aab1ba29cb58cc6d
wiky-avis <peta-holding@yandex.ru>
2021-04-27 15:23:27 +0300

    222

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -64,7 +64,6 @@ DATABASES = {
 }
 
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -79,7 +78,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 
 LANGUAGE_CODE = 'ru'
