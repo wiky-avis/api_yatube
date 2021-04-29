f5190c0074b7b79441bce303b3609cfa5dfba20d
wiky-avis <peta-holding@yandex.ru>
2021-04-29 16:11:07 +0300

    review 3

--- posts/urls.py
+++ posts/urls.py
@@ -5,14 +5,14 @@ from rest_framework.routers import DefaultRouter
 from .views import CommentViewSet, PostViewSet
 
 v1_router = DefaultRouter()
-v1_router.register('v1/posts', PostViewSet)
+v1_router.register('posts', PostViewSet)
 v1_router.register(
-    r'v1/posts/(?P<id>[0-9]+)/comments', CommentViewSet, basename='comments')
+    r'posts/(?P<id>[0-9]+)/comments', CommentViewSet, basename='comments')
 
 urlpatterns = v1_router.urls
 
 
 urlpatterns = [
-    path('', include(v1_router.urls)),
+    path('v1/', include(v1_router.urls)),
     path('v1/api-token-auth/', views.obtain_auth_token),
 ]
